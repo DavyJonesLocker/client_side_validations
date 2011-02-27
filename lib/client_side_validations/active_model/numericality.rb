b524d5b22abc292a9f7cd9d25268fd8f6cb8fea8
Brian Cardarella <bcardarella@gmail.com>
2011-02-27 01:26:52 -0500

    Numericality

---
+++ lib/client_side_validations/active_model/numericality.rb
@@ -0,0 +1,29 @@
+module ClientSideValidations::ActiveModel
+  module Numericality
+
+    OPTION_MAP = {}
+
+    def self.included(base)
+      OPTION_MAP.merge!(base::CHECKS.keys.inject({}) { |hash, key| hash.merge!(key => key) })
+      OPTION_MAP.merge!(:numericality => :not_a_number, :only_integer => :not_an_integer)
+    end
+
+    def client_side_hash(model, attribute)
+      extra_options = options.except(*::ActiveModel::Errors::CALLBACKS_OPTIONS).reject { |key, value| key == :only_integer && !value }
+      keys = [:numericality] | extra_options.keys
+      messages = keys.inject({}) do |hash, key|
+        count = extra_options[key]
+        hash.merge!(key => model.errors.generate_message(attribute, OPTION_MAP[key], :count => count))
+      end
+
+      { :messages => messages }.merge(extra_options)
+    end
+
+    private
+
+    def message_types
+      [:numericality]
+    end
+  end
+end
+
