b56545172e8a5f1739d7fa73d4938ada8c32bab2
Brian Cardarella <bcardarella@gmail.com>
2011-03-05 12:45:21 -0500

    Added without to format validator

--- test/active_model/cases/test_format_validator.rb
+++ test/active_model/cases/test_format_validator.rb
@@ -7,6 +7,11 @@ class ActiveModel::FormatValidatorTest < ClientSideValidations::ActiveModelTestB
     assert_equal expected_hash, FormatValidator.new(:attributes => [:name], :with => /.+/).client_side_hash(@person, :age)
   end
 
+  def test_format_client_side_hash_without
+    expected_hash = { :message => "is invalid", :without => /.+/ }
+    assert_equal expected_hash, FormatValidator.new(:attributes => [:name], :without => /.+/).client_side_hash(@person, :age)
+  end
+
   def test_format_client_side_hash_with_custom_message
     expected_hash = { :message => "is wrong format", :with => /.+/ }
     assert_equal expected_hash, FormatValidator.new(:attributes => [:name], :with => /.+/, :message => "is wrong format").client_side_hash(@person, :age)
