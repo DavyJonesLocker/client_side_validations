848fd848f74fa1c33468908034ed4d461c55a368
Brian Cardarella <bcardarella@gmail.com>
2011-03-07 02:57:55 -0500

    Better string wrappers

--- lib/generators/templates/client_side_validations.rb
+++ lib/generators/templates/client_side_validations.rb
@@ -2,12 +2,11 @@
 #
 # Uncomment the following block if you want each input field to have the validation messages attached.
 
-
 # ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
 #   unless html_tag =~ /^<label/
-#     "<div class=\"field_with_errors\">#{html_tag}<label for=\"#{instance.send(:tag_id)}\" class=\"message\">#{instance.error_message.first}</label></div>".html_safe
+#     %{<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>}.html_safe
 #   else
-#     "<div class=\"field_with_errors\">#{html_tag}</div>".html_safe
+#     %{<div class="field_with_errors">#{html_tag}</div>}.html_safe
 #   end
 # end
 
