536699cddd022feb93e2be4faa45e28156c90162
Brian Cardarella <bcardarella@gmail.com>
2011-03-25 21:52:43 -0400

    Removed unecessary code

--- test/javascript/public/test/settings.js
+++ test/javascript/public/test/settings.js
@@ -1,25 +1,3 @@
-var App = App || {};
-
-App.assert_callback_invoked = function(callback_name) {
-  ok(true, callback_name + ' callback should have been invoked');
-};
-
-App.assert_callback_not_invoked = function(callback_name) {
-  ok(false, callback_name + ' callback should not have been invoked');
-};
-
-App.assert_get_request = function(request_env){
-  equal(request_env['REQUEST_METHOD'], 'GET', 'request type should be GET');
-};
-
-App.assert_post_request = function(request_env){
-  equal(request_env['REQUEST_METHOD'], 'POST', 'request type should be POST');
-};
-
-App.assert_request_path = function(request_env, path) {
-  equal(request_env['PATH_INFO'], path, 'request should be sent to right url');
-};
-
 // hijacks normal form submit; lets it submit to an iframe to prevent
 // navigating away from the test suite
 $(document).bind('submit', function(e) {
