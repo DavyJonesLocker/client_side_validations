ef0953ac19b757c4f8c549802ae276086a644600
Brian Cardarella <bcardarella@gmail.com>
2011-05-04 02:37:45 -0400

    Performance refactor

--- lib/client_side_validations/active_model.rb
+++ lib/client_side_validations/active_model.rb
@@ -4,7 +4,7 @@ module ClientSideValidations::ActiveModel
   module Validator
 
     def client_side_hash(model, attribute)
-      extra_options = options.except(*::ActiveModel::Errors::CALLBACKS_OPTIONS - [:on, :allow_blank])
+      extra_options = options.except(*::ActiveModel::Errors::CALLBACKS_OPTIONS - [:allow_blank])
       { :message => model.errors.generate_message(attribute, message_type, extra_options) }.merge(extra_options)
     end
 
@@ -17,26 +17,34 @@ module ClientSideValidations::ActiveModel
 
   module Validations
     def client_side_validation_hash
-      _validators.except(nil, :block).inject({}) do |attr_hash, attr|
+      @client_side_validation_hash ||= _validators.inject({}) do |attr_hash, attr|
+        unless [nil, :block].include?(attr[0])
 
-        validator_hash = attr[1].inject({}) do |kind_hash, validator|
-          client_side_hash = validator.client_side_hash(self, attr[0])
-          # Yeah yeah, #new_record? is not part of ActiveModel :p
-          if (can_use_for_client_side_validation?(client_side_hash, validator))
-            kind_hash.merge!(validator.kind => client_side_hash.except(:on))
-          else
-            kind_hash.merge!({})
+          validator_hash = attr[1].inject({}) do |kind_hash, validator|
+            client_side_hash = validator.client_side_hash(self, attr[0])
+            # Yeah yeah, #new_record? is not part of ActiveModel :p
+            if (can_use_for_client_side_validation?(client_side_hash, validator))
+              kind_hash.merge!(validator.kind => client_side_hash.except(:on))
+            else
+              kind_hash.merge!({})
+            end
           end
-        end
 
-        attr_hash.merge!(attr[0] => validator_hash)
-      end.delete_if { |key, value| value.blank? }
+          if validator_hash.present?
+            attr_hash.merge!(attr[0] => validator_hash)
+          else
+            attr_hash
+          end
+        else
+          attr_hash
+        end
+      end
     end
 
     private
 
     def can_use_for_client_side_validation?(client_side_hash, validator)
-      ((self.respond_to?(:new_record?) && client_side_hash[:on] == (self.new_record? ? :create : :update)) || client_side_hash[:on].nil?) && !validator.options.key?(:if) && !validator.options.key?(:unless) && validator.kind != :block
+      ((self.respond_to?(:new_record?) && validator.options[:on] == (self.new_record? ? :create : :update)) || validator.options[:on].nil?) && !validator.options.key?(:if) && !validator.options.key?(:unless) && validator.kind != :block
     end
   end
 end
--- lib/client_side_validations/mongoid/uniqueness.rb
+++ lib/client_side_validations/mongoid/uniqueness.rb
@@ -1,18 +1,15 @@
 module ClientSideValidations::Mongoid
   module Uniqueness
     def client_side_hash(model, attribute)
-      extra_options = options.except(*::ActiveModel::Errors::CALLBACKS_OPTIONS - [:on, :allow_blank])
-      hash = { :message => model.errors.generate_message(attribute, message_type, extra_options.except(:case_sensitive, :scope)) }
-      hash = hash.merge(extra_options).merge(model.new_record? ? {} : { :id => model.id })
-
-      if hash[:scope].present?
-        hash[:scope] = Array.wrap(hash[:scope]).inject({}) do |scope_hash, scope_item|
+      hash = {}
+      hash[:message] = model.errors.generate_message(attribute, message_type, options.except(:scope))
+      hash[:case_sensitive] = options[:case_sensitive] if options.key?(:case_sensitive)
+      hash[:id] = model.id unless model.new_record?
+      if options.key?(:scope) && options[:scope].present?
+        hash[:scope] = Array.wrap(options[:scope]).inject({}) do |scope_hash, scope_item|
           scope_hash.merge!(scope_item => model.send(scope_item))
         end
-      else
-        hash.delete(:scope)
       end
-
       hash
     end
 
