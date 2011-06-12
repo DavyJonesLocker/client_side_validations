04cad3e39c83f097726f4729c78df4221c85941f
Brian Cardarella <bcardarella@gmail.com>
2011-06-12 16:30:50 -0400

    Order by alpha

--- lib/client_side_validations/files.rb
+++ lib/client_side_validations/files.rb
@@ -2,7 +2,7 @@
 
 module ClientSideValidations
   module Files
-    Javascript  = File.expand_path(File.dirname(__FILE__) + '/../../vendor/assets/javascripts/rails.validations.rb')
     Initializer = File.expand_path(File.dirname(__FILE__) + '/../generators/templates/client_side_validations/initializer.rb')
+    Javascript  = File.expand_path(File.dirname(__FILE__) + '/../../vendor/assets/javascripts/rails.validations.rb')
   end
 end
