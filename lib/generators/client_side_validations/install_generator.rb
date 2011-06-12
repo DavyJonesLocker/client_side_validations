9b0b218cedc86d307e0b0fd77cc18f1a1339c2a7
Brian Cardarella <bcardarella@gmail.com>
2011-06-12 12:54:50 -0400

    3.1 and 3.0 generators living together, mass hysteria!

--- lib/generators/client_side_validations/install_generator.rb
+++ lib/generators/client_side_validations/install_generator.rb
@@ -1,20 +1,32 @@
 module ClientSideValidations
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../../templates', __FILE__)
+      source_root File.expand_path('../../templates/client_side_validations', __FILE__)
 
-      desc 'Creates a ClientSideValidations initializer and copies client-side-validations.js to public/javascripts.'
-
-      def copy_initializer
-        copy_file 'client_side_validations/initializer.rb', 'config/initializers/client_side_validations.rb'
+      if Rails.version >= '3.1'
+        desc 'Creates a ClientSideValidations initializer.'
+      else
+        desc 'Creates a ClientSideValidations initializer and copies rails.validations.js to public/javascripts.'
       end
 
-      def copy_locale
-        copy_file '../../../vendor/assets/javascripts/rails.validations.js', 'public/javascripts/rails.validations.js'
+      def copy_initializer
+        copy_file 'initializer.rb', 'config/initializers/client_side_validations.rb'
+      end
+
+      def copy_javascript_asset
+        if Rails.version >= '3.0'
+          if Rails.version < '3.1'
+            copy_file '../../../../vendor/assets/javascripts/rails.validations.js', 'public/javascripts/rails.validations.js'
+          end
+        end
       end
 
       def show_readme
-        readme 'client_side_validations/README' if behavior == :invoke
+        if Rails.version >= '3.1'
+          readme 'README.rails.3.1' if behavior == :invoke
+        else
+          readme 'README.rails.3.0' if behavior == :invoke
+        end
       end
     end
   end
