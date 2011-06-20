ebb122c00534f594ad634b190134c542fb8671f8
Brian Cardarella <bcardarella@gmail.com>
2011-06-20 11:01:39 -0400

    Bumpd to RC 2

--- lib/client_side_validations/version.rb
+++ lib/client_side_validations/version.rb
@@ -1,3 +1,3 @@
 module ClientSideValidations
-  VERSION = '3.1.0.beta.4'
+  VERSION = '3.1.0.rc.2'
 end
--- vendor/assets/javascripts/rails.validations.js
+++ vendor/assets/javascripts/rails.validations.js
@@ -1,5 +1,5 @@
 /*!
- * Rails 3 Client Side Validations - v3.1.0.beta.1
+ * Rails 3 Client Side Validations - v3.1.0.rc.2
  * https://github.com/bcardarlela/client_side_validations
  *
  * Copyright (c) 2011 Brian Cardarella
