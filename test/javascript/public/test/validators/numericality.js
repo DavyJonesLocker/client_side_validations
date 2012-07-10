d7d9172ef3ea0ed8f7a3c79052d6a7e1363f4017
Brian Cardarella <bcardarella@gmail.com>
2012-07-09 22:35:00 -0400

    Gloriously hidden test gems!

--- test/javascript/public/test/validators/numericality.js
+++ test/javascript/public/test/validators/numericality.js
@@ -54,115 +54,115 @@ test('when only allowing integers and value has a negative or positive sign', fu
   equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
 });
 
-// test('when only allowing integers and value is not integer', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { only_integer: "failed validation", numericality: "failed validation" }, only_integer: true };
-  // element.val('123.456');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing integers and value is not integer', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { only_integer: "failed validation", numericality: "failed validation" }, only_integer: true };
+  element.val('123.456');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing values greater than 10 and value is greater than 10', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { greater_than: "failed validation", numericality: "failed validation" }, greater_than: 10 };
-  // element.val('11');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
-// });
+test('when only allowing values greater than 10 and value is greater than 10', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { greater_than: "failed validation", numericality: "failed validation" }, greater_than: 10 };
+  element.val('11');
+  equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
+});
 
-// test('when only allowing values greater than 10 and value is 10', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { greater_than: "failed validation", numericality: "failed validation" }, greater_than: 10 };
-  // element.val('10');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing values greater than 10 and value is 10', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { greater_than: "failed validation", numericality: "failed validation" }, greater_than: 10 };
+  element.val('10');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing values greater than or equal to 10 and value is 10', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { greater_than_or_equal_to: "failed validation", numericality: "failed validation" }, greater_than_or_equal_to: 10 };
-  // element.val('10');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
-// });
+test('when only allowing values greater than or equal to 10 and value is 10', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { greater_than_or_equal_to: "failed validation", numericality: "failed validation" }, greater_than_or_equal_to: 10 };
+  element.val('10');
+  equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
+});
 
-// test('when only allowing values greater than or equal to 10 and value is 9', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { greater_than_or_equal_to: "failed validation", numericality: "failed validation" }, greater_than_or_equal_to: 10 };
-  // element.val('9');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing values greater than or equal to 10 and value is 9', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { greater_than_or_equal_to: "failed validation", numericality: "failed validation" }, greater_than_or_equal_to: 10 };
+  element.val('9');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing values less than 10 and value is less than 10', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { less_than: "failed validation", numericality: "failed validation" }, less_than: 10 };
-  // element.val('9');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
-// });
+test('when only allowing values less than 10 and value is less than 10', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { less_than: "failed validation", numericality: "failed validation" }, less_than: 10 };
+  element.val('9');
+  equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
+});
 
-// test('when only allowing values less than 10 and value is 10', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { less_than: "failed validation", numericality: "failed validation" }, less_than: 10 };
-  // element.val('10');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing values less than 10 and value is 10', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { less_than: "failed validation", numericality: "failed validation" }, less_than: 10 };
+  element.val('10');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing values less than or equal to 10 and value is 10', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { less_than_or_equal_to: "failed validation", numericality: "failed validation" }, less_than_or_equal_to: 10 };
-  // element.val('10');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
-// });
+test('when only allowing values less than or equal to 10 and value is 10', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { less_than_or_equal_to: "failed validation", numericality: "failed validation" }, less_than_or_equal_to: 10 };
+  element.val('10');
+  equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
+});
 
-// test('when only allowing values less than or equal to 10 and value is 11', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { less_than_or_equal_to: "failed validation", numericality: "failed validation" }, less_than_or_equal_to: 10 };
-  // element.val('11');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing values less than or equal to 10 and value is 11', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { less_than_or_equal_to: "failed validation", numericality: "failed validation" }, less_than_or_equal_to: 10 };
+  element.val('11');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing values equal to 10 and value is 10', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { equal_to: "failed validation", numericality: "failed validation" }, equal_to: 10 };
-  // element.val('10');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
-// });
+test('when only allowing values equal to 10 and value is 10', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { equal_to: "failed validation", numericality: "failed validation" }, equal_to: 10 };
+  element.val('10');
+  equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
+});
 
-// test('when only allowing values equal to 10 and value is 11', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { equal_to: "failed validation", numericality: "failed validation" }, equal_to: 10 };
-  // element.val('11');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing values equal to 10 and value is 11', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { equal_to: "failed validation", numericality: "failed validation" }, equal_to: 10 };
+  element.val('11');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing value equal to 0 and value is 1', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { equal_to: "failed validation", numericality: "failed validation" }, equal_to: 0 };
-  // element.val('1');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing value equal to 0 and value is 1', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { equal_to: "failed validation", numericality: "failed validation" }, equal_to: 0 };
+  element.val('1');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing odd values and the value is odd', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { odd: "failed validation", numericality: "failed validation" }, odd: true };
-  // element.val('11');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
-// });
+test('when only allowing odd values and the value is odd', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { odd: "failed validation", numericality: "failed validation" }, odd: true };
+  element.val('11');
+  equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
+});
 
-// test('when only allowing odd values and the value is even', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { odd: "failed validation", numericality: "failed validation" }, odd: true };
-  // element.val('10');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing odd values and the value is even', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { odd: "failed validation", numericality: "failed validation" }, odd: true };
+  element.val('10');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
-// test('when only allowing even values and the value is even', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { even: "failed validation", numericality: "failed validation" }, even: true };
-  // element.val('10');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
-// });
+test('when only allowing even values and the value is even', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { even: "failed validation", numericality: "failed validation" }, even: true };
+  element.val('10');
+  equal(ClientSideValidations.validators.local.numericality(element, options), undefined);
+});
 
-// test('when only allowing even values and the value is odd', function() {
-  // var element = $('<input type="text" />');
-  // var options = { messages: { even: "failed validation", numericality: "failed validation" }, even: true };
-  // element.val('11');
-  // equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
-// });
+test('when only allowing even values and the value is odd', function() {
+  var element = $('<input type="text" />');
+  var options = { messages: { even: "failed validation", numericality: "failed validation" }, even: true };
+  element.val('11');
+  equal(ClientSideValidations.validators.local.numericality(element, options), "failed validation");
+});
 
