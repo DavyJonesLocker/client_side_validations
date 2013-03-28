4504f97e4b99b787cd187e45734057ddcf75fc0e
Max Barnash <i.am@anhero.ru>
2013-03-29 05:53:52 +0700

    Fixed test

--- test/javascript/public/test/utilities.js
+++ test/javascript/public/test/utilities.js
@@ -2,7 +2,7 @@ module('Utilities');
 
 test('Remote Validator Url without setting', function() {
   ClientSideValidations.remote_validators_prefix = undefined;
-  equal(ClientSideValidations.remote_validators_url_for('test'), '/validators/test');
+  equal(ClientSideValidations.remote_validators_url_for('test'), '//'+window.location.host'/validators/test');
 });
 
 test('Remote Validator Url with setting', function() {
