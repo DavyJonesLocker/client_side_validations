c159f78b4e9420fd3ef6cf49a0fdb39e59b55558
Pandurang <pandurang.plw@gmail.com>
2014-03-01 10:39:46 +0530

    Option to display error field along with error message

--- lib/generators/templates/client_side_validations/initializer.rb
+++ lib/generators/templates/client_side_validations/initializer.rb
@@ -6,16 +6,19 @@
 # Uncomment to validate number format with current I18n locale
 # ClientSideValidations::Config.number_format_with_locale = true
 
+# Uncomment to display error field along with error message
+# ClientSideValidations::Config.show_error_field = true
+
 # Uncomment the following block if you want each input field to have the validation messages attached.
 #
 # Note: client_side_validation requires the error to be encapsulated within
 # <label for="#{instance.send(:tag_id)}" class="message"></label>
 #
 # ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+#   error_field = ClientSideValidations::Config.show_error_field ? "#{instance.method_name.capitalize} " : ""
 #   unless html_tag =~ /^<label/
-#     %{<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>}.html_safe
+#     %{<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{error_field}#{instance.error_message.first}</label></div>}.html_safe
 #   else
 #     %{<div class="field_with_errors">#{html_tag}</div>}.html_safe
 #   end
 # end
-
