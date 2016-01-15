4f000000a858a46d830a4b2aec1e8d6f41f58685
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-01-15 18:58:26 +0100

    Regenerated JavaScript

--- vendor/assets/javascripts/rails.validations.js
+++ vendor/assets/javascripts/rails.validations.js
@@ -1,6 +1,6 @@
 
 /*!
- * Client Side Validations - v4.2.0 (https://github.com/DavyJonesLocker/client_side_validations)
+ * Client Side Validations - v4.2.1 (https://github.com/DavyJonesLocker/client_side_validations)
  * Copyright (c) 2016 Brian Cardarella
  * Licensed under MIT (http://opensource.org/licenses/mit-license.php)
  */
