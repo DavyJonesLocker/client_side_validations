1be4434d7f448240aba0bc2ef0c7e9a11265030d
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-20 19:36:46 +0100

    Use a module to include validations

--- lib/client_side_validations/active_record.rb
+++ lib/client_side_validations/active_record.rb
@@ -1,11 +1,9 @@
 require 'client_side_validations/active_model'
+require 'client_side_validations/extender'
 require 'client_side_validations/middleware'
 require 'client_side_validations/active_record/middleware'
 
 ActiveRecord::Base.send(:include, ClientSideValidations::ActiveModel::Validations)
 ClientSideValidations::Middleware::Uniqueness.register_orm(ClientSideValidations::ActiveRecord::Middleware)
 
-%w(Uniqueness).each do |validator|
-  require "client_side_validations/active_record/#{validator.downcase}"
-  ActiveRecord::Validations.const_get("#{validator}Validator").send :include, ClientSideValidations::ActiveRecord.const_get(validator)
-end
+ClientSideValidations::Extender.extend 'ActiveRecord', %w(Uniqueness)
---
+++ lib/client_side_validations/extender.rb
@@ -0,0 +1,12 @@
+module ClientSideValidations
+  module Extender
+    module_function
+
+    def extend(klass, validators)
+      validators.each do |validator|
+        require "client_side_validations/#{klass.underscore}/#{validator.downcase}"
+        const_get(klass)::Validations.const_get("#{validator}Validator").send :include, ClientSideValidations.const_get(klass).const_get(validator)
+      end
+    end
+  end
+end
