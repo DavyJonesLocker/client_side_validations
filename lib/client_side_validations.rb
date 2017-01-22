80ca606a7415ab49cb7c1cafb963ee8169b0ec66
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-22 16:04:42 +0100

    Add back Engine
    Engine is needed to register assets

--- lib/client_side_validations.rb
+++ lib/client_side_validations.rb
@@ -3,4 +3,7 @@ require 'client_side_validations/active_model'  if defined?(::ActiveModel)
 require 'client_side_validations/active_record' if defined?(::ActiveRecord)
 require 'client_side_validations/action_view'   if defined?(::ActionView)
 
-require 'client_side_validations/generators'    if defined?(::Rails)
+if defined?(::Rails)
+  require 'client_side_validations/engine'
+  require 'client_side_validations/generators'
+end
---
+++ lib/client_side_validations/engine.rb
@@ -0,0 +1,4 @@
+module ClientSideValidations
+  class Engine < ::Rails::Engine
+  end
+end
