9335504ae6d49b0f43d4f7c0f1e649cb69d1fa6a
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-30 18:20:19 +0100

    Improve initializer

--- lib/generators/templates/client_side_validations/initializer.rb
+++ lib/generators/templates/client_side_validations/initializer.rb
@@ -11,10 +11,10 @@
 # Note: client_side_validation requires the error to be encapsulated within
 # <label for="#{instance.send(:tag_id)}" class="message"></label>
 #
-# ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-#   unless html_tag =~ /^<label/
-#     %{<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>}.html_safe
+# ActionView::Base.field_error_proc = proc do |html_tag, instance|
+#   if html_tag =~ /^<label/
+#     %(<div class="field_with_errors">#{html_tag}</div>).html_safe
 #   else
-#     %{<div class="field_with_errors">#{html_tag}</div>}.html_safe
+#     %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>).html_safe
 #   end
 # end
