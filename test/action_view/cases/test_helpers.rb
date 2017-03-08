1cd7e79eee016426b8086d54864ce4d8bae8787c
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-03-08 11:22:40 +0100

    Remove encoding magic comments
    UTF-8 is the default Ruby 2.0 encoding and we only support Ruby >= 2.2

--- test/action_view/cases/test_helpers.rb
+++ test/action_view/cases/test_helpers.rb
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 require 'action_view/cases/helper'
 
 module ClientSideValidations
--- test/action_view/models/format_thing.rb
+++ test/action_view/models/format_thing.rb
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 class FormatThing
   extend ActiveModel::Naming
   extend ActiveModel::Translation
