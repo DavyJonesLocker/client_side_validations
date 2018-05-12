797d411c64e82a3f9f81bb3bd299bfc1a2a5c106
Jonathan Fleckenstein <jonfleck88@gmail.com>
2018-05-12 09:18:23 -0400

    Update to new rollup api.

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,48 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
 
 export default [
-	// browser-friendly UMD build
-	{
-		entry: 'src/main.js',
-		dest: pkg.browser,
-		format: 'umd',
-		moduleName: 'howLongUntilLunch',
-		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs(), // so Rollup can convert `ms` to an ES module
-			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	},
+  // browser-friendly UMD build
+  {
+    input: 'src/main.js',
+    external: ['jquery'],
+    output: [
+      {
+        file: pkg.browser,
+        format: 'umd',
+        name: 'clientSideValidations',
+        globals: {
+          jquery: '$'
+        }
+      }
+    ],
+    plugins: [
+      resolve(), // so Rollup can find `jquery`
+      commonjs(), // so Rollup can convert `jquery` to an ES module
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// the `targets` option which can specify `dest` and `format`)
-	{
-		entry: 'src/main.js',
-		external: ['ms'],
-		targets: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	}
-];
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // the `targets` option which can specify `dest` and `format`)
+  {
+    input: 'src/main.js',
+    external: ['jquery'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  }
+]
