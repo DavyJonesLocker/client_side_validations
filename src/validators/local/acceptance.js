67e0ef8152db3fa16f55d7e417a62fcb1cbcc2aa
Geremia Taglialatela <tagliala.dev@gmail.com>
2019-08-02 22:28:44 +0200

    Fix acceptance validator
    Match Rails 5’s default behavior

--- src/validators/local/acceptance.js
+++ src/validators/local/acceptance.js
@@ -1,3 +1,24 @@
+import { arrayHasValue } from '../../helpers.js'
+
+const DEFAULT_ACCEPT_OPTION = ['1', true]
+
+Array.isArray || (Array.isArray = (a) => {
+  const object = {}
+  return ('' + a) !== a && object.toString.call(a) === '[object Array]'
+})
+
+const isTextAccepted = (value, acceptOption) => {
+  if (!acceptOption) {
+    acceptOption = DEFAULT_ACCEPT_OPTION
+  }
+
+  if (Array.isArray(acceptOption)) {
+    return arrayHasValue(value, acceptOption)
+  }
+
+  return value === acceptOption
+}
+
 export const acceptanceLocalValidator = (element, options) => {
   var valid = true
 
@@ -6,9 +27,7 @@ export const acceptanceLocalValidator = (element, options) => {
   }
 
   if (element.attr('type') === 'text') {
-    // TODO: fix this validator. It does not work with arrays
-    const accept = options.accept || '1'
-    valid = element.val() === accept.toString()
+    valid = isTextAccepted(element.val(), options.accept)
   }
 
   if (!valid) {
