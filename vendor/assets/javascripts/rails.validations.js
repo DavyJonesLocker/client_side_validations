24a029c5c74a9844b55ee5d6ad47f367a39e62d3
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-01-09 16:13:02 +0100

    Regenerate files

--- vendor/assets/javascripts/rails.validations.js
+++ vendor/assets/javascripts/rails.validations.js
@@ -1,6 +1,6 @@
 /*!
  * Client Side Validations JS - v0.1.5 (https://github.com/DavyJonesLocker/client_side_validations)
- * Copyright (c) 2020 Geremia Taglialatela, Brian Cardarella
+ * Copyright (c) 2021 Geremia Taglialatela, Brian Cardarella
  * Licensed under MIT (https://opensource.org/licenses/mit-license.php)
  */
 
