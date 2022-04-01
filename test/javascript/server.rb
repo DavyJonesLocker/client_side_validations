1f1758c8f102e747c406b92ade745698bf16b347
Geremia Taglialatela <tagliala.dev@gmail.com>
2022-04-01 19:25:07 +0200

    Update QUnit

--- test/javascript/server.rb
+++ test/javascript/server.rb
@@ -28,7 +28,7 @@ use AssetPath, urls: ['/vendor/assets/javascripts'], root: File.expand_path('../
 use AssetPath, urls: ['/vendor/assets/javascripts'], root: File.expand_path('../', $LOAD_PATH.find { |p| p.include?('jquery-rails') })
 
 DEFAULT_JQUERY_VERSION = '3.6.0'
-QUNIT_VERSION          = '2.17.2'
+QUNIT_VERSION          = '2.18.1'
 
 helpers do
   def jquery_version
