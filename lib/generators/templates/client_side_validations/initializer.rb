d6b82cd1325919ac3786f17391a06aa19ab17b6c
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-11-17 18:56:53 +0100

    Improve initializer template
    Suggest to use `Regexp#match?` instead of `string =~ Regexp`

--- lib/generators/templates/client_side_validations/initializer.rb
+++ lib/generators/templates/client_side_validations/initializer.rb
@@ -16,7 +16,7 @@
 # <label for="#{instance.send(:tag_id)}" class="message"></label>
 #
 # ActionView::Base.field_error_proc = proc do |html_tag, instance|
-#   if html_tag =~ /^<label/
+#   if /^<label/.match?(html_tag)
 #     %(<div class="field_with_errors">#{html_tag}</div>).html_safe
 #   else
 #     %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>).html_safe
